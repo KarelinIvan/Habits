b23f19c0914c6ba05ffd1752db4035000cde002a
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-12-09 15:13:15 +0300

    3-й коммит, вьюсеты для приложения пользователи

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView
 
 from users.models import Users
 from users.serializer import UserSerializer
@@ -6,5 +6,28 @@ from users.serializer import UserSerializer
 
 class UserCreateAPIView(CreateAPIView):
     """ Регистрация нового пользователя """
+    serializer_class = UserSerializer
+    queryset = Users.objects.all()
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    """ Изменение профиля пользователя """
+    serializer_class = UserSerializer
+    queryset = Users.objects.all()
+
+
+class UserDeleteAPIView(DestroyAPIView):
+    """ Удаление профиля пользователя """
+    queryset = Users.objects.all()
+
+
+class UserListAPIView(ListAPIView):
+    """ Для просмотра списка профилей """
+    serializer_class = UserSerializer
+    queryset = Users.objects.all()
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    """ Просмотр пользователя по ID """
     serializer_class = UserSerializer
     queryset = Users.objects.all()
