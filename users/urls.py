27fd0428866e299fb137cd546a1ba73059d10b51
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-12-09 15:20:45 +0300

    4-й коммит, урлы

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserCreateAPIView
+from users.views import UserCreateAPIView, UserUpdateAPIView, UserDeleteAPIView, UserListAPIView, UserRetrieveAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('', UserListAPIView.as_view(), name='user_list'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
+    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
+    path('delete/<int:pk>/', UserDeleteAPIView.as_view(), name='user_delete'),
+    path('viewing/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_viewing')
 ]
