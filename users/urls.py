94be3947eee0d0bd49b1d95e0544a0b2fbf6ef27
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-12-09 16:45:55 +0300

    6-й коммит, токен

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserCreateAPIView, UserUpdateAPIView, UserDeleteAPIView, UserListAPIView, UserRetrieveAPIView
+from users.views import UserCreateAPIView, UserUpdateAPIView, UserDeleteAPIView, UserListAPIView, UserRetrieveAPIView, \
+    UserTokenObtainPairView
 
 app_name = UsersConfig.name
 
@@ -10,5 +13,7 @@ urlpatterns = [
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
     path('delete/<int:pk>/', UserDeleteAPIView.as_view(), name='user_delete'),
-    path('viewing/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_viewing')
+    path('viewing/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_viewing'),
+    path('login/', UserTokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,35 +2,51 @@ from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView
 
 from users.models import Users
 from users.serializer import UserSerializer, UserTokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 class UserCreateAPIView(CreateAPIView):
     """ Регистрация нового пользователя """
     serializer_class = UserSerializer
     queryset = Users.objects.all()
+    permission_classes = [AllowAny]
 
 
 class UserUpdateAPIView(UpdateAPIView):
     """ Изменение профиля пользователя """
     serializer_class = UserSerializer
     queryset = Users.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def password_update(self, serializer):
+        """ Обновление и сохранение пароля, если он был изменен """
+        # Сохранение данных пользователя
+        user = serializer.save()
+        # Установка нового пароля
+        user.set_password(self.request.data.get("password"))
+        # Сохранение обновленных данных
+        user.save()
 
 
 class UserDeleteAPIView(DestroyAPIView):
     """ Удаление профиля пользователя """
     queryset = Users.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class UserListAPIView(ListAPIView):
     """ Для просмотра списка профилей """
     serializer_class = UserSerializer
     queryset = Users.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class UserRetrieveAPIView(RetrieveAPIView):
     """ Просмотр пользователя по ID """
     serializer_class = UserSerializer
     queryset = Users.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class UserTokenObtainPairView(TokenObtainPairView):
